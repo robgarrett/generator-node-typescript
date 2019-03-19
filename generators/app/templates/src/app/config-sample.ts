4bc29d5e7d13abe50766a74cbf70959e047df68f
Rob Garrett <rob@robgarrett.com>
2019-03-19 18:43:51 -0400

    Renamed exports

--- generators/app/templates/src/app/config-sample.ts
+++ generators/app/templates/src/app/config-sample.ts
@@ -1,4 +1,5 @@
-export module GraphConfig {
+export module OAuthConfig {
+  // Exports for OAUTH using client ID and secret.
   export const clientId = "Add client id here";
   export const clientSecret = "Add client secret here";
 }
